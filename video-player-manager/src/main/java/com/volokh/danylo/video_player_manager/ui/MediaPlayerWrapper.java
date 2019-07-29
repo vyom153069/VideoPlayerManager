7be225425a773f4d37ec90a97a2855d04cc4c572
vyom chandra gallani <vyomchandra@gmail.com>
2019-07-29 17:18:33 +0530

    'this should be open in main thread '  fixed

--- video-player-manager/src/main/java/com/volokh/danylo/video_player_manager/ui/MediaPlayerWrapper.java
+++ video-player-manager/src/main/java/com/volokh/danylo/video_player_manager/ui/MediaPlayerWrapper.java
@@ -669,6 +669,7 @@ public abstract class MediaPlayerWrapper
     }
 
     private boolean inUiThread() {
-        return Thread.currentThread().getId() == 1;
+        return Thread.currentThread() == Looper.getMainLooper().getThread();
     }
-}
+    }
+
